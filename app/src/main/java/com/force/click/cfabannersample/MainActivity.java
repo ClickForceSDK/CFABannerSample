98a5ea90f76692a150d8c176c426129d8f817faf
Charles <YaoChang@guoyaozhangde-MacBook-Pro.local>
2017-09-21 15:36:02 +0800

    Update Android SDK 1.2.2

--- app/src/main/java/com/force/click/cfabannersample/MainActivity.java
+++ app/src/main/java/com/force/click/cfabannersample/MainActivity.java
@@ -19,7 +19,7 @@ public class MainActivity extends AppCompatActivity {
         ad = (AdView) this.findViewById(R.id.ad);
         int weg = (int) ((this.getResources().getDisplayMetrics().widthPixels) * 0.6);
         int hig = (int) (weg * 0.15625);
-        ad.getAd(5053, weg, hig,320,50);
+        ad.getAd(3783, weg, hig,320,50);
 
 
         //Ad Load Callback
